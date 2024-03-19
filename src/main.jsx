7c841d5b85b9d6b1fd158342fda1a90e22694f3d
ArwynAnin <137463257+ArwynAnin@users.noreply.github.com>
2024-03-19 19:53:02 +0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Google from './Google'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <Google></Google>
+  </React.StrictMode>,
+)
